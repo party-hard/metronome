7eb9bb49259a8f55000843e090d69f1dbcb6368a
Andrey Yavtushenko <andrew.yavtushenko@gmail.com>
2018-06-12 21:23:18 +0200

    remove babel-preset-es2015 and install babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-              presets: ['react', 'es2015', 'stage-0'],
+              presets: ['env', 'react', 'stage-0'],
               plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
           }
         },
