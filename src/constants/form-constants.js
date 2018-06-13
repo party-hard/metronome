6256a73fb0fc13d04356fc801afce7b8e0d2b1eb
Andrew Yavtushenko <andrew@Andrews-MacBook-Pro.local>
2018-06-13 20:45:29 +0200

    edit note values selector

--- src/constants/form-constants.js
+++ src/constants/form-constants.js
@@ -1,20 +1,20 @@
 export default  {
     beatRange: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
     noteValueRange: [{
+        name: '1',
+        value: 1
+    },{
+        name: '2',
+        value: 2
+    },{
         name: '4',
         value: 4
     },{
         name: '8',
         value: 8
     },{
-        name: '8 triplet',
-        value: 12
-    },{
         name: '16',
         value: 16
-    },{
-        name: '16 triplet',
-        value: 24
     }],
     defaultBeat: 4,
     defaultNoteValue: {
